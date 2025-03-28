0aa8886d7dd19d81516add696a514207c8929edd
Gabriely <159936039+gabrielysantos8@users.noreply.github.com>
2025-03-28 20:07:23 -0300

    Add files via upload

--- aula05/index.php
+++ aula05/index.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
     <div class="container">
         <form action="processa.php" method="post">
             <!-- Email input -->
             <div data-mdb-input-init class="form-outline mb-4">
+                <label class="form-label" for="form2Example1">Endereço de email</label>
                 <input type="email" id="form2Example1" class="form-control"  name="email"/>
-                <label class="form-label" for="form2Example1">Email address</label>
             </div>
 
             <!-- Password input -->
             <div data-mdb-input-init class="form-outline mb-4">
+                <label class="form-label" for="form2Example2">senha</label>
                 <input type="password" id="form2Example2" class="form-control" name="senha"/>
-                <label class="form-label" for="form2Example2">Password</label>
             </div>
 
             <!-- 2 column grid layout for inline styling -->
@@ -27,41 +29,42 @@
                 <!-- Checkbox -->
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                    <label class="form-check-label" for="form2Example31"> Remember me </label>
+                    <label class="form-check-label" for="form2Example31"> Lembre-me </label>
                 </div>
                 </div>
 
                 <div class="col">
                 <!-- Simple link -->
-                <a href="#!">Forgot password?</a>
+                <a href="#!">Esqueceu a senha?</a>
                 </div>
             </div>
 
             <!-- Submit button -->
-             <input type="submit" value="Sign in" class="btn btn-primary btn-block mb-4">
+             <input type="submit" value="Entrar" class="btn btncolor btn-block mb-4 d-block mx-auto">
             <!-- <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button> -->
 
             <!-- Register buttons -->
             <div class="text-center">
-                <p>Not a member? <a href="#!">Register</a></p>
-                <p>or sign up with:</p>
+                <p>Não é membro? <a href="#!">Registre-se</a></p>
+                <p>ou entre com:</p>
                 <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-facebook-f"></i>
+                <i class="bi bi-facebook"></i>
                 </button>
 
                 <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-google"></i>
+                <i class="bi bi-google"></i>
                 </button>
 
                 <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-twitter"></i>
+                <i class="bi bi-twitter-x"></i>
                 </button>
 
                 <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-github"></i>
+                <i class="bi bi-github"></i>
                 </button>
             </div>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- aula05/processa.php
+++ aula05/processa.php
@@ -3,12 +3,9 @@
     $senha = $_POST['senha'];
 
     if ($email == "aluno@email.com" && $senha == "1234"){
-        //manda para autorizado.php
-        //echo "autorizado";
-        header('location: painel/autorizado.html');
+        $nome = "Aluno";
+        header('location: privada.php?nome='.$nome);
     }else{
-        //manda para naoautorizado.php
-        //echo "nao autorizado";
-        header('location: painel/naoautorizado.html');
+        header('location: erro.php');
     }
 ?>
