71dc6c25331d7d2ce92e3afefe96a1b16c540670
Gabriely <159936039+gabrielysantos8@users.noreply.github.com>
2025-05-21 12:41:25 -0300

    Add files via upload

---
+++ aula11/Produto.class.php
@@ -0,0 +1,45 @@
+<?php
+
+    class Produto{
+        private $Nome;
+        private $Preco;
+        private $Quantidade;
+
+        public function __construct($Nome, $Preco = 0.0, $Quantidade = 0){
+            $this->Nome = $Nome;
+            $this->Preco = $Preco;
+            $this->Quantidade = $Quantidade;
+        }
+
+        public function getNome(){
+            return $this->Nome;
+        }
+
+        public function setPreco($Preco){
+            if ($Preco > 0){
+                $this->Preco = $Preco;
+            }else{
+                echo "Preço inválido";
+            }
+        }
+
+        public function AdicionarEstoque($quant){
+            $this->Quantidade += $quant;
+            $result = $this->Quantidade;
+            return $result;
+        }
+
+        public function RemoverEstoque($quant){
+            $this->Quantidade -= $quant;
+            $result = $this->Quantidade;
+            return $result;
+        }
+
+        public function MostrarDetalhes(){
+            echo "Nome: " . $this->Nome . "<br>";
+            echo "Preço: " . $this->Preco . "<br>";
+            echo "Quantidade de Estoque: " . $this->Quantidade . "<br>";
+        }
+    }
+
+?>
